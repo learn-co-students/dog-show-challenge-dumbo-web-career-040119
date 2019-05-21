ee34d29698de3af864fd557371df777ca4ab5059
github username <github email address>
2019-05-21 15:11:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -40,38 +40,37 @@ function editDog(e){
     })
     .then(resp => resp.json())
     .then(data =>{
-        //loadDogs();
-        currenttd1.innerText = data.name;
-        currenttd2.innerText = data.breed;
-        currenttd3.innerText = data.sex; 
+        currenttd1.innerText = data.name; //updates selected name element
+        currenttd2.innerText = data.breed; //updates selected breed element
+        currenttd3.innerText = data.sex; //updates selected sex element
     })
 }
 
 function createElement(dog){
     let tr = document.createElement('tr');
     tr.className = 'padding';
-    let td1 = document.createElement('td');
-    td1.innerText = dog.name;
-    tr.appendChild(td1);
-    let td2 = document.createElement('td');
-    td2.innerText = dog.breed;
-    tr.appendChild(td2);
-    let td3 = document.createElement('td');
-    td3.innerText = dog.sex;
-    tr.appendChild(td3);
-    let td4 = document.createElement('td');
-    let button = document.createElement('button');
-    button.innerText = 'Edit';
-    button.addEventListener('click',function(){
-        dogNameField.value = dog.name;
-        dogBreedField.value = dog.breed;
-        dogSexField.value = dog.sex;
-        currentDog = dog;//instance of dog
-        currenttd1 = td1;//element
-        currenttd2 = td2;//element
-        currenttd3 = td3;//element
-    })
-    td4.appendChild(button);
+        let td1 = document.createElement('td');
+        td1.innerText = dog.name; //name
+        tr.appendChild(td1); 
+        let td2 = document.createElement('td');
+        td2.innerText = dog.breed; //breed
+        tr.appendChild(td2);
+        let td3 = document.createElement('td');
+        td3.innerText = dog.sex; //sex
+        tr.appendChild(td3);
+        let td4 = document.createElement('td');
+        let button = document.createElement('button');
+        button.innerText = 'Edit';
+        button.addEventListener('click',function(){
+            dogNameField.value = dog.name;
+            dogBreedField.value = dog.breed;
+            dogSexField.value = dog.sex;
+            currentDog = dog;//instance of dog
+            currenttd1 = td1;//element for name 
+            currenttd2 = td2;//element for breed
+            currenttd3 = td3;//element for sex
+        })
+        td4.appendChild(button);
     tr.appendChild(td4);
     tableBody.appendChild(tr);
 }
